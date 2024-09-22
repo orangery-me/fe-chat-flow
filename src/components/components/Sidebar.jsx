834f8386292f4208a0af2ab26a6eae985d0e6a5c
thuthanh <thuthanh0501@gmai.com>
2024-09-22 13:07:51 +0700

    thêm chatroom

--- src/components/components/Sidebar.jsx
+++ src/components/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import "./Sidebar.css"; // Custom CSS for styling
 import { useAuth } from "../../hooks/useAuth";
+import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import { chatRooms } from "../../data/chatRooms";
 const Sidebar = () => {
   const info = useAuth();
   // const [isLoggedIn, setIsLoggedIn] = useState(true); // Trạng thái đăng nhập
@@ -12,6 +15,17 @@ const Sidebar = () => {
   // const handleLogout = () => {
   //   setIsLoggedIn(false); // Cập nhật trạng thái chưa đăng nhập
   // };
+  const [isOverlayOpen, setOverlayOpen] = useState(false);
+
+  const openOverlay = () => {
+    setOverlayOpen(true);
+  };
+
+  const closeOverlay = () => {
+    setOverlayOpen(false);
+  };
+  const param = useParams();
+  const room = chatRooms.find((room) => room.id === param.id);
   return (
     <div className="sidebar">
       <div className="sidebar-item profile">
@@ -34,6 +48,87 @@ const Sidebar = () => {
           {/* Icon thông báo */}
           <i className="icon-bell"></i>
         </div>
+        <div className="sidebar-item">
+          <button
+            onClick={openOverlay}
+            style={{ background: "none", border: "none", cursor: "pointer" }}
+          >
+            <i
+              class="far fa-plus-square"
+              style={{ fontSize: "24px", color: "white" }}
+            ></i>
+          </button>
+        </div>
+        {isOverlayOpen && (
+          <div className="overlay">
+            <div className="overlay-content">
+              <div className="overlay-title">
+                <h4 style={{ marginTop: "0px" }}>Tạo nhóm</h4>
+                <button onClick={closeOverlay}>
+                  <i
+                    class="far fa-times-circle"
+                    style={{ fontSize: "24px" }}
+                  ></i>
+                </button>
+              </div>
+              <div
+                style={{
+                  border: "1px solid #ccc", // Đường viền màu xám, dày 2px
+                  borderRadius: "10px",
+                  marginTop: "20px",
+                }}
+              ></div>
+              <div className="nameroom">
+                <div class="circle-icon">
+                  <i
+                    class="fas fa-camera"
+                    style={{ fontSize: "24px", color: "#fffff" }}
+                  ></i>
+                </div>
+                <div className="name">
+                  <input type="text"></input>
+                  <div
+                    style={{
+                      border: "1px solid #6aaff4", // Đường viền màu xám, dày 2px
+                      borderRadius: "10px",
+                    }}
+                    className="line"
+                  ></div>
+                </div>
+              </div>
+              <div className="search">
+                <i
+                  class="fas fa-search"
+                  style={{
+                    color: "#ccc",
+                    paddingRight: "10px",
+                    paddingLeft: "7px",
+                  }}
+                ></i>
+                <input type="text" placeholder="Nhập email"></input>
+              </div>
+              <div
+                style={{
+                  border: "1px solid #ccc", // Đường viền màu xám, dày 2px
+                  borderRadius: "10px",
+                  marginTop: "20px",
+                }}
+              ></div>
+              <div className="list">
+                {chatRooms.map((room) => (
+                  <div className="group-overlay">
+                    <img src="" alt="avatar" className="imagine"></img>
+                    <div className="group-item">
+                      <div className="info" key={room.id}>
+                        {room.title}
+                      </div>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
         <div className="sidebar-item">
           {/* Icon cài đặt */}
           <i className="icon-settings"></i>
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App";
 import { AuthContext, AuthProvider } from "./context/AuthContext";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatRoom from "./components/ChatRoom/index";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
