4001d0fdb8db5d22e6d319e07fb77be095826218
Chau Thi <102220086@sv1.dut.udn.vn>
2024-12-20 12:40:50 +0700

    testing new file path

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "@/Context/AuthContext";
-import Profile from "@/components/MyProfile/profile";
-import Notification from "@/components/Notification/Notification";
-import { StompClientProvider } from "@/context/StompClientContext";
-import { WebRTCProvider } from "@/context/WebRTC";
-import ChatRoom from "@/components/ChatRoom/index";
-import CallRoom from "@/components/CallRoom/index";
+import Profile from "./components/MyProfile/profile";
+import Notification from "./components/Notification/Notification";
+import { StompClientProvider } from "./context/StompClientContext";
+import { WebRTCProvider } from "./context/WebRTC";
+import { AuthProvider } from "./context/AuthContext";
+import ChatRoom from "./components/ChatRoom/index";
+import CallRoom from "./components/CallRoom/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
