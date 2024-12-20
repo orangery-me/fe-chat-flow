0b1d0134ed129853c3fb8546f60491036e5b8f3c
Chau Thi <102220086@sv1.dut.udn.vn>
2024-12-20 12:15:53 +0700

    testing update in vite config

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import { AuthProvider } from "./Context/AuthContext";
+import { AuthProvider } from "@/Context/AuthContext";
 import Profile from "./components/MyProfile/profile";
 import Notification from "./components/Notification/Notification";
 import { StompClientProvider } from "./context/StompClientContext";
@@ -16,15 +16,15 @@ root.render(
   <React.StrictMode>
     <Router>
       <AuthProvider>
-          <StompClientProvider>
+        <StompClientProvider>
           <WebRTCProvider>
-              <Routes>
-                <Route path="/" element={<App />} />
-                <Route path="/chat/:roomId" element={<ChatRoom />} />
-                <Route path="/call/:roomId" element={<CallRoom />} />
-                <Route path="/myprofile" element={<Profile />} />
-                <Route path="/mynotification" element={<Notification />} />
-              </Routes>
+            <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/chat/:roomId" element={<ChatRoom />} />
+              <Route path="/call/:roomId" element={<CallRoom />} />
+              <Route path="/myprofile" element={<Profile />} />
+              <Route path="/mynotification" element={<Notification />} />
+            </Routes>
           </WebRTCProvider>
         </StompClientProvider>
       </AuthProvider>
--- vite.config.mjs
+++ vite.config.mjs
@@ -10,5 +10,10 @@ export default defineConfig({
     host: "0.0.0.0",
     port: 5173,
   },
+  resolve: {
+    alias: {
+      '@': '/src',
+    }
+  },
   plugins: [react()],
 });
