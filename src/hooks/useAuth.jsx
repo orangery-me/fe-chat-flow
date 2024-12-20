ec419c78ab6ab873b0fa70074a537ab5b5d79cd9
Chau Thi <102220086@sv1.dut.udn.vn>
2024-12-20 14:19:48 +0700

    test 4

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AuthContext } from '../Context/AuthContext';
+import { AuthContext } from '../context/AuthContext';
 
 function useAuth () {
     const value = React.useContext(AuthContext);
